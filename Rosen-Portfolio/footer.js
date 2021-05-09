eeaa6c9a0a449f9a61df527b055d3a75b4f1f09e
Travis-Rosen <tmrosen@my365.bellevue.edu>
2021-05-09 18:47:16 -0500

    Minor changes to code

--- Rosen-Portfolio/footer.js
+++ Rosen-Portfolio/footer.js
@@ -14,10 +14,10 @@ class Footer extends HTMLElement
                 <h3>Travis Rosen</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                 <ul class="socials">
-                    <li><a href="#"><i class="fab fa-github"></i></a></li>
-                    <li><a href="#"><i class="fab fa-github"></i></a></li>
+                    <li><a href="https://github.com/Travis-Rosen?tab=repositories" target="_blank"><i class="fab fa-github"></i></a></li>
+                    <li><a href="https://github.com/buwebdev/web-330" target="_blank"><i class="fab fa-github"></i></a></li>
                     <li><a href="#"><i class="fab fa-youtube"></i></a></li>
-                    <li><a href="#"><i class="fas fa-university"></i></a></li>
+                    <li><a href="https://www.bellevue.edu/degrees/bachelor/web-development-bs/" target="_blank"><i class="fas fa-university"></i></a></li>
                 </ul>
             </div>
             <div class="footer-bottom">
