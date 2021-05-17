b48dfb478f0f0c17123de3958733ad169e361aa2
Travis-Rosen <tmrosen@my365.bellevue.edu>
2021-05-16 23:20:23 -0500

    Final Changes to Portfolio

--- Rosen-Portfolio/footer.js
+++ Rosen-Portfolio/footer.js
@@ -1,3 +1,4 @@
+//JavaScript that will extend onto html page and display the footer listed below.
 class Footer extends HTMLElement 
 {
     constructor()
@@ -12,7 +13,7 @@ class Footer extends HTMLElement
         <footer>
             <div class="footer-content">
                 <h3>Travis Rosen</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <p>Below you can find links to my personal GitHub, Bellevue University GitHub, Bellevue Youtube, and Web Development @ Bellevue University.</p>
                 <ul class="socials">
                     <li><a href="https://github.com/Travis-Rosen?tab=repositories" target="_blank"><i class="fab fa-github"></i></a></li>
                     <li><a href="https://github.com/buwebdev/web-330" target="_blank"><i class="fab fa-github"></i></a></li>
@@ -21,6 +22,7 @@ class Footer extends HTMLElement
                 </ul>
             </div>
             <div class="footer-bottom">
+                <p><a href="#">Return to top<a></p>
                 <p>Copyright &copy;2021 Travis Rosen - Bellevue University</p>
             </div>
         </footer>
--- Rosen-Portfolio/header.js
+++ Rosen-Portfolio/header.js
@@ -1,3 +1,4 @@
+//JavaScript that will extend onto html page and display the navbar listed below.
 class Header extends HTMLElement 
 {
     constructor() 
@@ -8,45 +9,16 @@ class Header extends HTMLElement
     connectedCallback() {
         this.innerHTML = `
         <header>
-            <nav class = "navbar navbar-expand-lg navbar-light">
-                <div class = "collapse navbar-collapse">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a href="index.html" class="nav-link">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="about.html" class="nav-link">About</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="resume.html" class="nav-link">Resume</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="projects.html" class="nav-link">Projects</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="api.html" class="nav-link">API Tests</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="diagrams.html" class="nav-link">DataBase Diagrams</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="bucket-list.html" class="nav-link">BucketList</a>
-                        </li>
-                    <ul>
-                </div>
-            </nav>
-        </header>
-
-
-
-
-
-
-
-
-
-
-        `
+            <div id="navbar">
+                <a href="index.html" class="nav-link">Home</a>
+                <a href="about.html" class="nav-link">About</a>
+                <a href="resume.html" class="nav-link">Resume</a>
+                <a href="projects.html" class="nav-link">Projects</a>
+                <a href="api.html" class="nav-link">API Tests</a>
+                <a href="diagrams.html" class="nav-link">DataBase Diagrams</a>
+                <a href="bucket-list.html" class="nav-link">BucketList</a>
+            </div>
+        </header> `
     }
 }
 
